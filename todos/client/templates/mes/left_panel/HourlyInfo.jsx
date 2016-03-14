0ea17394c69b59a54a876325b5c04ee121cc5a23
Deepti Mota <deepti@systango.com>
2016-03-14 18:07:49 +0530

    changed the table for hourly info table style

--- todos/client/templates/mes/left_panel/HourlyInfo.jsx
+++ todos/client/templates/mes/left_panel/HourlyInfo.jsx
@@ -9,13 +9,15 @@ HourlyInfo = React.createClass({
     dataRecords.forEach(function(record){
       var time = record.recordTime;
       var hour = time.getHours().toString();
-      if (info[hour]) {
-        info[hour] += record.count;
-      }
-      else {
-        info[hour] = 1;
-      }
-      accumulativeCount += record.count;
+      if (record.count){
+        if (info[hour]) {
+          info[hour] += record.count;
+        }
+        else {
+          info[hour] = 1;
+        }
+        accumulativeCount += record.count;
+      }  
     })
     return {
       dataRecords : dataRecords,
@@ -54,35 +56,53 @@ HourlyInfo = React.createClass({
       }
   },
 
+  createMatrix : function (rows, columns)  {
+    this.rows = rows;
+    this.columns = columns;
+    this.myarray = new Array(this.rows);
+    for (var i=0; i < this.columns; i +=1) {
+        this.myarray[i]=new Array(this.rows)
+    }
+    return this.myarray;
+  },
+
   plotInfo : function(){
     var info = this.data.info
     var all_keys = Object.keys(info)
     var hour_details = []
     var three_info = []
     var count = 0;
+    var final_info = this.createMatrix(2,2);
     all_keys.map(function(key){
-      count += 1;
       var int_key = parseInt(key)
-      var final_info =  key +":00 -"+(int_key + 1).toString()+ ":00"+ " :- "; 
-      three_info.push(<span key={Random.id()}>{final_info} <b>{info[key].toString()},</b> </span>)
-      if (count == 3)
-      {
-        three_info.push(<br key={Random.id()}/>);
+      final_info[count][0] = key +":00 -"+(int_key + 1).toString()+ ":00"+ " : "; 
+      final_info[count][1] =  info[key].toString();
+      count += 1
+      if (count == 2){
+        three_info.push(<tr key={Random.id()}> <td key={Random.id()}>{final_info[0][0]}<b>{final_info[0][1]}</b></td>  <td key={Random.id()}>{final_info[1][0]}<b>{final_info[1][1]}</b></td> </tr>);
         count = 0;
+        final_info = this.createMatrix(2,2);  
       }
-    })
+    }.bind(this));
+    if(final_info[0][0]){
+      three_info.push(<tr key={Random.id()}> <td key={Random.id()}>{final_info[0][0]}<b>{final_info[0][1]}</b></td></tr>);
+    }
     return three_info
   },
 
   render : function(){
     var yaxis = this.getHourlyInfoDivPos();
     return(
-      <div ref="overlay" style={{"top": yaxis + "px","left": this.props.x + "px"}} className="ShopFloorInfoBox">
+      <div ref="overlay" style={{"top": yaxis + "px","left": this.props.x + "px"}} className="ShopFloorInfoBox z-depth-5">
         <div className="hourlyinfo_lft"> {this.props.levelName} </div>
         <a onClick={this.closePopUp} className="hourlyinfo_rgt"><font color="black">X</font></a>
         <div className="clr_div">
-          {this.plotInfo()}
-          <p> 累计产量 : <b>{this.data.accumulativeCount}</b> </p>
+        <table className="striped centered z-depth-1">  
+          <tbody>
+            {this.plotInfo()}
+          </tbody>
+        </table>
+        <p> 累计产量 : <b>{this.data.accumulativeCount}</b> </p>
         </div>
       </div>
     )
--- todos/client/templates/mes/middle_dashboard/work_center_info.jsx
+++ todos/client/templates/mes/middle_dashboard/work_center_info.jsx
@@ -5,11 +5,11 @@ WorkCenterInfo = React.createClass({
     var pos_y = this.props.pos_y;
     var dashboard_div = document.getElementById('application-content');
 
-    if ((this.props.pos_x + 100) < (dashboard_div.offsetWidth - 260)) {
+    if ((this.props.pos_x + 100) < (dashboard_div.offsetWidth - 300)) {
       pos_x = this.props.pos_x + 100
     }
     else{
-      pos_x = this.props.pos_x - 260
+      pos_x = this.props.pos_x - 300
     }
 
     if ((this.props.pos_y + 420) > dashboard_div.offsetHeight) {
@@ -49,30 +49,30 @@ WorkCenterInfo = React.createClass({
       return value;
     };
     return(
-      <div className="WorkCenterInfoBox" style = {{"left": pos[0] + 'px',"top": pos[1] + 'px', "width": "260"}}>
+      <div className="WorkCenterInfoBox" style = {{"left": pos[0] + 'px',"top": pos[1] + 'px', "width": "300"}}>
         {this.props.info_stats.last_item ?
           <table>
             <tbody>
               <tr className="bg_dark"> <td className="wci_align_right">欄位名稱</td> <td>值</td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">狀態</td> <td> {getStatusInChinese(this.props.info_stats.last_item.currentStatus)} </td> </tr>
-              <tr> <td className="wci_align_right">工作中心</td> <td> {checkValue(this.props.info_stats.last_item.workcenterName)} </td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">機器名稱</td> <td> {checkValue(this.props.info_stats.last_item.machineName)} </td> </tr>
-              <tr> <td className="wci_align_right">工作單</td><td>  {checkValue(this.props.info_stats.last_item.workorderNo)} </td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">工序號</td> <td> {checkValue(this.props.info_stats.last_item.sequenceNo)} </td> </tr>
-              <tr> <td className="wci_align_right">物料編碼</td> <td> {checkValue(this.props.info_stats.last_item.partno)}</td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">物料名稱</td> <td> {checkValue(this.props.info_stats.last_item.partnoName)}</td> </tr>
-              <tr> <td className="wci_align_right">設備編號</td> <td> {checkValue(this.props.info_stats.last_item.deviceNo)} </td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">開始時間</td> <td> {moment(this.props.info_stats.last_item.startTime).format("MMM Do h:mm:s")} </td> </tr>
-              <tr> <td className="wci_align_right">最後生產時間</td> <td> {moment(this.props.info_stats.last_item.recordTime).format("MMM Do h:mm:s")} </td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">不良數</td> <td> {checkValue(this.props.info_stats.NGCount)} </td> </tr>
-              <tr> <td className="wci_align_right">累計生產數</td> <td> {checkValue(this.props.info_stats.accumulativeCount)} </td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">平均輸出(小時)</td> <td> {checkValue(this.props.info_stats.avg_output)} </td> </tr>
-              <tr> <td className="wci_align_right">標準輸出(小時):</td> <td> {checkValue(this.props.info_stats.standard_work_time)} </td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">當前效率</td> <td> {checkValue(this.props.info_stats.currentEfficiency)} </td> </tr>
-              <tr> <td className="wci_align_right">當天效率</td> <td> {checkValue(this.props.info_stats.todayEfficiency)} </td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">當前品質達標率</td> <td> {checkValue(this.props.info_stats.currentQualityRate)} </td> </tr>
-              <tr> <td className="wci_align_right">當天品質達標率</td> <td> {checkValue(this.props.info_stats.todayQualityRate)} </td> </tr>
-              <tr className="bg_grey"> <td className="wci_align_right">功能名称</td> <td> {checkValue(this.props.info_stats.last_item.functionName)} </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">狀態</td> <td className="info_box_align"> {getStatusInChinese(this.props.info_stats.last_item.currentStatus)} </td> </tr>
+              <tr> <td className="wci_align_right">工作中心</td> <td className="info_box_align"> {checkValue(this.props.info_stats.last_item.workcenterName)} </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">機器名稱</td> <td className="info_box_align"> {checkValue(this.props.info_stats.last_item.machineName)} </td> </tr>
+              <tr> <td className="wci_align_right">工作單</td><td className="info_box_align">  {checkValue(this.props.info_stats.last_item.workorderNo)} </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">工序號</td> <td className="info_box_align"> {checkValue(this.props.info_stats.last_item.sequenceNo)} </td> </tr>
+              <tr> <td className="wci_align_right">物料編碼</td> <td className="info_box_align"> {checkValue(this.props.info_stats.last_item.partno)}</td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">物料名稱</td> <td className="info_box_align"> {checkValue(this.props.info_stats.last_item.partnoName)}</td> </tr>
+              <tr> <td className="wci_align_right">設備編號</td> <td className="info_box_align"> {checkValue(this.props.info_stats.last_item.deviceNo)} </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">開始時間</td> <td className="info_box_align"> {moment(this.props.info_stats.last_item.startTime).format("MMM Do h:mm:s")} </td> </tr>
+              <tr> <td className="wci_align_right">最後生產時間</td> <td className="info_box_align"> {moment(this.props.info_stats.last_item.recordTime).format("MMM Do h:mm:s")} </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">不良數</td> <td className="info_box_align"> {checkValue(this.props.info_stats.NGCount)} </td> </tr>
+              <tr> <td className="wci_align_right">累計生產數</td> <td className="info_box_align"> {checkValue(this.props.info_stats.accumulativeCount)} </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">平均輸出(小時)</td> <td className="info_box_align"> {checkValue(this.props.info_stats.avg_output)} </td> </tr>
+              <tr> <td className="wci_align_right">標準輸出(小時):</td> <td className="info_box_align"> {checkValue(this.props.info_stats.standard_work_time)} </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">當前效率</td> <td className="info_box_align"> {checkValue(this.props.info_stats.currentEfficiency)}{this.props.info_stats.currentEfficiency ? '%' : ''} </td> </tr>
+              <tr> <td className="wci_align_right">當天效率</td> <td className="info_box_align"> {checkValue(this.props.info_stats.todayEfficiency)}{this.props.info_stats.todayEfficiency ? '%' : '' } </td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">當前品質達標率</td> <td className="info_box_align"> {checkValue(this.props.info_stats.currentQualityRate)}{this.props.info_stats.currentQualityRate ? '%' : ''} </td> </tr>
+              <tr> <td className="wci_align_right">當天品質達標率</td> <td className="info_box_align"> {checkValue(this.props.info_stats.todayQualityRate)}{this.props.info_stats.todayQualityRate ? '%' :''}</td> </tr>
+              <tr className="bg_grey"> <td className="wci_align_right">功能名称</td> <td className="info_box_align"> {checkValue(this.props.info_stats.last_item.functionName)} </td> </tr>
             </tbody>
           </table>
         :""}
